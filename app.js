7cc7e8c1c8f8c00e32ae1710fec9f48dc94867cd
Junosoft <junosoftltd@gmail.com>
2022-09-27 22:32:21 +0200

    port is edited

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = expr();
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.post("/",(req,res)=>{
-  req.body.n
-})
-const port = process.env.port || 3000
-app.listen(port,()=>console.log("server is running on ",port));
+app.post("/", (req, res) => {
+  req.body.n;
+});
+const port = process.env.PORT || 3000 ;
+app.listen(port, () => console.log(`server is running on  ${port}`));
